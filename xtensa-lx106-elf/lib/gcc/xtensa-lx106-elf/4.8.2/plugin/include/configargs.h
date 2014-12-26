f76350c4b74906b4c1f8e15bfca67e7793bd02ee
Vysakh P Pillai <vysakhpillai@gmail.com>
2014-12-26 17:00:37 +0530

    toolchain v0.1. Untested

---
+++ xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/media/90GBDrive/esp8266/crosstool-NG/.build/src/gcc-4.8.2/configure --build=x86_64-build_unknown-linux-gnu --host=arm-host_buildroot-linux-uclibcgnueabi --target=xtensa-lx106-elf --prefix=/media/90GBDrive/esp8266/crosstool-NG/builds/xtensa-lx106-elf --with-local-prefix=/media/90GBDrive/esp8266/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/sysroot --disable-libmudflap --with-sysroot=/media/90GBDrive/esp8266/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/sysroot --with-newlib --enable-threads=no --disable-shared --with-pkgversion='crosstool-NG 1.20.0' --disable-__cxa_atexit --with-gmp=/media/90GBDrive/esp8266/crosstool-NG/.build/xtensa-lx106-elf/buildtools/complibs-host --with-mpfr=/media/90GBDrive/esp8266/crosstool-NG/.build/xtensa-lx106-elf/buildtools/complibs-host --with-mpc=/media/90GBDrive/esp8266/crosstool-NG/.build/xtensa-lx106-elf/buildtools/complibs-host --with-isl=/media/90GBDrive/esp8266/crosstool-NG/.build/xtensa-lx106-elf/buildtools/complibs-host --with-cloog=/media/90GBDrive/esp8266/crosstool-NG/.build/xtensa-lx106-elf/buildtools/complibs-host --with-libelf=/media/90GBDrive/esp8266/crosstool-NG/.build/xtensa-lx106-elf/buildtools/complibs-host --enable-lto --enable-target-optspace --disable-libgomp --disable-libmudflap --disable-nls --disable-multilib --enable-languages=c";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
